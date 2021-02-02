a96cdfb0bdea5f80feada20aef67dc0b399bb6d7
Danilo D. Vaz <danilodvaz@gmail.com>
2021-02-01 23:27:51 -0300

    Adicionado exemplo de SoftDeletes

--- app/Brand.php
+++ app/Brand.php
@@ -3,9 +3,15 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Brand extends Model
 {
+    // Permite "apagar" um itemdo do BD, mas sem realmente apagar.
+    // Ele vai marcar o item como apagado, fazendo com que o Model não o encontre mais.
+    // É necessário utilizar a função "softDeletes()" no migration, para preparar a tabela.
+    use SoftDeletes;
+
     protected $fillable = [
         'name'
     ];
